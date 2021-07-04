eb11e120971c6ed9bfa7fc05d645d5b062f8f1e7
kandarp dangi <kandarpdangi@gmail.com>
2021-07-04 22:55:38 +0530

    initiating for next 11

--- pages/api/product/[pid].js
+++ pages/api/product/[pid].js
@@ -3,27 +3,25 @@ import initDB from '../../../helpers/initDB'
 
 initDB()
 
-export default async (req,res)=>{
-    switch(req.method){
-        case "GET":
-          await getProduct(req,res) 
-          break; 
-        case "DELETE":
-          await deleteProduct(req,res)
-          break;
-    }
-    
+export const product = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await getProduct(req, res)
+      break
+    case 'DELETE':
+      await deleteProduct(req, res)
+      break
+  }
 }
 
-
-const getProduct = async (req,res)=>{
-    const {pid } =  req.query
-     const product = await Product.findOne({_id:pid})
-     res.status(200).json(product)
+const getProduct = async (req, res) => {
+  const { pid } = req.query
+  const product = await Product.findOne({ _id: pid })
+  res.status(200).json(product)
 }
 
-const deleteProduct = async (req,res)=>{
-    const {pid } =  req.query
-    await Product.findByIdAndDelete({_id:pid})
-    res.status(200).json({})
+const deleteProduct = async (req, res) => {
+  const { pid } = req.query
+  await Product.findByIdAndDelete({ _id: pid })
+  res.status(200).json({})
 }
