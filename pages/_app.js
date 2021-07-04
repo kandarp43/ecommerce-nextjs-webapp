a682f3d123b5f94f6e8ade8d0fe60319ac7e184b
kandarp dangi <kandarpdangi@gmail.com>
2021-07-04 23:23:43 +0530

    initiating for next 11

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Layout>
-          <Component {...pageProps} />  
-        </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
 
-
-  export default MyApp
+export default MyApp
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 
-const Product = ()=>{
-    return(
-      <div>
-        <h1>product page</h1>
-        <Link href="/"><a>go to home</a></Link>
-      </div>
-    )
-  }
-  
-  export default Product
+const Product = () => {
+  return (
+    <div>
+      <h1>product page</h1>
+      <Link href='/'>
+        <a>go to home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Product
