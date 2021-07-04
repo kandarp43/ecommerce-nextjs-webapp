888f1fdcb1a5bcf5e54464b1fa250dc23c5797bb
kandarp dangi <kandarpdangi@gmail.com>
2021-07-04 22:45:55 +0530

    init

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,2 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://mystore202144.vercel.app": "http://localhost:3000"
+const baseUrl =
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
 export default baseUrl
